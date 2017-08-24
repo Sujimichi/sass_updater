24154340bf8c9467c3cae17c398f424995ddfea4
Sujimichi <sujimichi@gmail.com>
2017-08-24 17:36:14 +0100

    enable block to be passed to optionally show line by line counts

--- lib/sass_updater/processor.rb
+++ lib/sass_updater/processor.rb
@@ -6,7 +6,9 @@ class Processor
     @data = [args].flatten
   end
   
-  def update_sass
+  def update_sass &block
+    @block = block
+    @count = 0
     @updated = @data.map do |line|
       update_line line
     end
@@ -16,7 +18,12 @@ class Processor
     return line unless line.lstrip.match(/^:/)  #ignore all lines which don't have : as the first non-whitespace char
     white_space, command = line.split(":", 2)   #separate the preceeding whitespace from the rest of the command (removes the leading :)
     attribute, value = command.split(" ", 2)    #split the command into the attribute and it's value    
-    "#{white_space}#{attribute}:#{value.nil? ? '' : ' ' + value}" #reconstruct with the : in the correct place
+    new_line = "#{white_space}#{attribute}:#{value.nil? ? '' : ' ' + value}" #reconstruct with the : in the correct place
+    @block.call(@count, line, new_line) if @block
+    @count+=1 if @count
+
+
+    new_line
   end
     
 end 
